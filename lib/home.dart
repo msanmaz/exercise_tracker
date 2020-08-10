032707bab3551c0a1606f1fa2aa38f2188e49bab
msanmaz <osanmaz4992@gmail.com>
2020-08-10 01:45:33 +0100

    ui changes 2.0

--- lib/home.dart
+++ lib/home.dart
@@ -1,31 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+const iconGym = 'assets/images/exercise.svg';
+
 class Home extends StatelessWidget {
+  Color hexToColor(String code) {
+    return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
+  }
+
+  Widget get topSection => Container(
+        height: 100.0,
+        color: Colors.yellow[300],
+      );
+
+  Widget get actionToolBar => Container(
+        width: 100.0,
+        color: Colors.red[300],
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: List<Widget>.generate(
+                5,
+                (_) => Container(
+                      width: 60,
+                      height: 80,
+                      color: Colors.blue[300],
+                      margin: EdgeInsets.only(bottom: 20.0, top: 20.0),
+                    ))),
+      );
+
+  Widget get mainFeed => Expanded(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            height: 10.0,
+            color: Colors.green,
+            margin: EdgeInsets.only(top: 10),
+          ),
+          Container(
+            height: 10.0,
+            color: Colors.green,
+            margin: EdgeInsets.only(top: 10),
+          )
+        ],
+      ));
+
+  Widget get middleSection => Expanded(
+      child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[mainFeed, actionToolBar]));
+
+  Widget get bottomSectiom => Container(
+        height: 80.0,
+        color: Colors.blue[300],
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         //top section
-        children: <Widget>[
-          Container(
-            height: 100.0,
-            color: Colors.yellow[300],
-          ),
-
-          //middle section
-          Expanded(
-              child: Row(children: <Widget>[
-            Expanded(child: Container(color: Colors.green[300])),
-            Container(
-              width: 100.0,
-              color: Colors.red[300],
-            )
-          ])),
-
-          //bottom section
-          Container(height: 80.0, color: Colors.blue[300])
-        ],
+        children: <Widget>[topSection, middleSection, bottomSectiom],
       ),
     );
   }
